af6d85840df7449b45f54dcd9028e00441550e52
Oketch <oketchngwani@gmail.com>
2025-01-18 04:43:13 +0900

    phase-1-array-map-method-lab

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "what is the constructor oo pattern?",
+  "implementing blockchain web api",
+  "the test driven development workflow",
+  "what is nan and how can we check for it",
+  "what is the difference between stoppropagation and preventdefault?",
+  "immutable state and pure functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is jsonp?"
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => {
+    return tutorial
+      .split(' ')
+      .map(word => word[0].toUpperCase() + word.slice(1))
+      .join(' ');
+  });
 }
